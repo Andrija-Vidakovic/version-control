c98a50c62d983d08f3520d6ecb73422212fee8ff
Andrija Vidakovic <andrija.vidakovic@gmail.com>
2023-12-31 16:52:41 +0100

    modified:   test.js

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 (function () {
-  console.log("Zdravo svete!");
+  console.log("Zdravo svete!!");
 })();
 
-let regx = /".+?"/g;
+let regx = /".+?"/g; 
 let regx1 = /"[^"]+"/g
 let str = 'a "witch" and her "broom" is "test"one';
 console.log(str.match(regx1));
