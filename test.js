397b4f43449f683a29a8b1b1e674e19f59131282
Andrija-Vidakovic <andrija.vidakovic@gmail.com>
2023-12-31 14:17:49 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+(function () {
+  console.log("Zdravo svete!");
+})();
+
+let regx = /".+?"/g;
+let regx1 = /"[^"]+"/g
+let str = 'a "witch" and her "broom" is "test"one';
+console.log(str.match(regx1));
